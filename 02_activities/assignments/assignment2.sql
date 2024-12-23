e8e065736e32483fe793b09b3995fec666bbfb7b
anisess10 <bouwazra.anis@gmail.com>
2024-12-23 12:45:07 -0500

    Section1&2

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -20,30 +20,73 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
+SELECT 
+    COALESCE(product_name, '') || ', ' || 
+    COALESCE(product_size, '') || ' (' || 
+    COALESCE(product_qty_type, 'unit') || ')' AS List_of_Products
+FROM product;
 
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
 visits to the farmer’s market (labeling each market date with a different number). 
-Each customer’s first visit is labeled 1, second visit is labeled 2, etc. 
+Each customer’s first visit is labeled 1, second visit is labeled 2, etc. */
 
+select   
+	*,
+	row_number() OVER (PARTITION by customer_id  order by market_date asc) as Vist_N
+from customer_purchases 
+
+/*
 You can either display all rows in the customer_purchases table, with the counter changing on
 each new market date for each customer, or select only the unique market dates per customer 
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
+SELECT DISTINCT 
+    customer_id, 
+    market_date,
+    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date ASC) AS Visit_N
+FROM customer_purchases;
 
+/* Using the DENSE_RANK() method */
+
+SELECT   
+    *,
+    DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY market_date ASC) AS Visit_N
+FROM customer_purchases;
 
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+SELECT DISTINCT 
+    customer_id, 
+    market_date,
+    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date DESC) AS Visit_N
+FROM customer_purchases;
+
+/* then write another query that uses this one as a subquery (or temp table) and filters the results to 
+only the customer’s most recent visit  */
+
+SELECT *
+FROM (
+    SELECT DISTINCT 
+        customer_id, 
+        market_date,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date DESC) AS Visit_N
+    FROM customer_purchases
+) x
+WHERE x.Visit_N = 1;
 
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
 
-
+SELECT 
+	*,
+    count(*) OVER (PARTITION BY customer_id, product_id ORDER BY product_id DESC) AS Count_Purchase
+FROM customer_purchases;
 
 -- String manipulations
 /* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
@@ -57,11 +100,17 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
-
+select product_name, 
+	case 
+		when INSTR(product_name, '-') > 0 then TRIM( SUBSTR(product_name, INSTR(product_name, '-') + 1)) else null 
+	END AS description
+from product
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
 
+select * from product 
+where product_size REGEXP '[0-9]';
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
@@ -73,7 +122,25 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
-
+select *
+From (
+	 Select
+	market_date, 
+	 sum(quantity* cost_to_customer_per_qty) as Total_Sales
+	from customer_purchases
+	Group By market_date
+	Order by Total_Sales DESC
+	limit 1)
+union
+select *
+From (
+	 Select
+	market_date, 
+	 sum(quantity* cost_to_customer_per_qty) as Total_Sales
+	from customer_purchases
+	Group By market_date
+	Order by Total_Sales ASC
+	limit 1)
 
 
 /* SECTION 3 */
